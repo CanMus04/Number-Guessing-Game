c5809550e31feeed68f7cc4adbd94495272dfc8f
Can Mustafa <mustafacan2004.jbs@gmail.com>
2025-05-09 19:42:40 +0200

    number_guessing_game.py

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -4,19 +4,18 @@ print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 print("Try to guess it!")
 
-# Computer chooses a random number
+
 secret_number = random.randint(1, 100)
 attempts = 0
 guessed = False
 
-# Game loop
+
 while not guessed:
-    # User input
+   
     try:
         guess = int(input("\nYour guess: "))
         attempts += 1
         
-        # Check the guess
         if guess < 1 or guess > 100:
             print("Please enter a number between 1 and 100.")
         elif guess < secret_number:
